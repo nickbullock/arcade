4ad2fbf33743dbaf5876826c221ce2f47c9b4931
nickbullock <balakirevnikita@yandex.ru>
2017-12-24 13:43:39 +0300

    added ragdoll

---
+++ Assets/Scripts/Player/BulletGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Invector.CharacterController
+{
+    public class BulletGenerator : MonoBehaviour
+    {
+        public GameObject bulletPrefab;
+        public GameObject fireParticlePrefab;
+        public Transform bulletSpawn;
+        
+        public float bulletSpeed;
+        public float fireRate;
+        public float damage;
+        public float range;
+        protected float timer;
+        
+        protected ParticleSystem gunParticles;
+        protected Ray shootRay;
+        protected RaycastHit shootHit;
+        protected int shootMask;
+
+        protected virtual void Start()
+        {
+            timer = 0;
+            gunParticles = GetComponent<ParticleSystem>();
+            shootMask = LayerMask.GetMask ("Player");
+            shootRay = new Ray();
+        }
+
+        protected void Update()
+        {
+            Debug.DrawRay(bulletSpawn.position, gameObject.transform.forward * 10, Color.red);
+        }
+
+        public void Generate()
+        {
+            timer = timer + 100 * Time.deltaTime;
+
+            if (bulletPrefab && bulletSpawn && timer > fireRate)
+            {
+                shootRay.origin = bulletSpawn.position;
+                shootRay.direction = gameObject.transform.forward;
+                
+                gunParticles.Stop();
+                gunParticles.Play();
+                
+                Quaternion rotation = Quaternion.Euler(
+                    90,
+                    gameObject.transform.parent.rotation.eulerAngles.y,
+                    gameObject.transform.parent.rotation.eulerAngles.z);
+
+                GameObject bullet = (GameObject) Instantiate(
+                    bulletPrefab,
+                    bulletSpawn.position,
+                    rotation);
+                
+                timer = timer - fireRate;
+
+                bullet.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * bulletSpeed;
+
+                if (Physics.Raycast(shootRay, out shootHit, range, shootMask))
+                {
+                    vThirdPersonController enemyController = shootHit.collider.GetComponent<vThirdPersonController>();
+                    if (enemyController != null)
+                    {
+                        Destroy(bullet, 0.1f);
+                        enemyController.Damage(damage, shootHit.point);
+                    }
+                }
+                else
+                {
+                    Destroy(bullet, 2.0f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/vThirdPersonController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace Invector.CharacterController
+{
+    public class vThirdPersonController : vThirdPersonAnimator
+    {
+        protected virtual void Start()
+        {
+#if !UNITY_EDITOR
+                Cursor.visible = false;
+#endif
+        }
+
+        public virtual void Sprint(bool value)
+        {
+            isSprinting = value;
+        }
+
+        public virtual void Strafe()
+        {
+            if (locomotionType == LocomotionType.OnlyFree) return;
+            isStrafing = !isStrafing;
+        }
+
+        public virtual void Jump()
+        {
+            // conditions to do this action
+            bool jumpConditions = isGrounded && !isJumping;
+            // return if jumpCondigions is false
+            if (!jumpConditions) return;
+            // trigger jump behaviour
+            jumpCounter = jumpTimer;
+            isJumping = true;
+            // trigger jump animations            
+            if (_rigidbody.velocity.magnitude < 1)
+                animator.CrossFadeInFixedTime("Jump", 0.1f);
+            else
+                animator.CrossFadeInFixedTime("JumpMove", 0.2f);
+        }
+
+        public virtual void PickUp()
+        {
+            if (itemAround)
+            {
+                pickUpItem = itemAround;
+                weaponIKIsActive = true;
+                
+                pickUpItem.transform.parent = gameObject.transform;
+                
+                pickUpItem.transform.localPosition = new Vector3(-0.2f, 1f, 1f);
+                pickUpItem.transform.rotation = gameObject.transform.rotation;
+                
+                Rigidbody itemRigidbody = pickUpItem.GetComponent<Rigidbody>();
+                BoxCollider itemCollider = pickUpItem.GetComponent<BoxCollider>();
+
+                itemRigidbody.useGravity = false;
+                itemRigidbody.isKinematic = true;
+                itemCollider.enabled = false;
+            }
+        }
+
+        public virtual void Drop()
+        {
+            if (pickUpItem)
+            {
+                weaponIKIsActive = false;
+                
+                Rigidbody itemRigidbody = pickUpItem.GetComponent<Rigidbody>();
+                BoxCollider itemCollider = pickUpItem.GetComponent<BoxCollider>();
+                
+                itemRigidbody.useGravity = true;
+                itemRigidbody.isKinematic = false;
+                itemCollider.enabled = true;
+
+                pickUpItem.transform.parent = null;
+                pickUpItem = null;
+            }
+        }
+
+        public virtual void Fire()
+        {
+            if (pickUpItem && pickUpItem.CompareTag("Weapon"))
+            {
+                pickUpItem.GetComponent<BulletGenerator>().Generate();
+            }
+        }
+
+        public virtual void Damage(float hp, Vector3 hitPoint)
+        {
+            health = health - hp;
+
+            if (health <= 0)
+            {
+               Die();
+            }
+        }
+
+        public virtual void Die()
+        {
+            Debug.Log("DEAD");
+            
+            DisableRagdoll(false);
+            GetComponent<Animator>().enabled=false;
+//            Destroy(gameObject, 5);
+      
+        }
+
+        public virtual void Turn()
+        {
+#if !MOBILE_INPUT
+            // Create a ray from the mouse cursor on screen in the direction of the camera.
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(camRay.origin, camRay.direction * 10, Color.green);
+
+            // Create a RaycastHit variable to store information about what was hit by the ray.
+            RaycastHit floorHit;
+
+            // Perform the raycast and if it hits something on the floor layer...
+            if (Physics.Raycast(camRay, out floorHit, 100f, groundLayer))
+            {
+                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+                Vector3 playerToMouse = floorHit.point - transform.position;
+
+                // Ensure the vector is entirely along the floor plane.
+                playerToMouse.y = 0f;
+
+                // Set the player's rotation to this new rotation.
+                _rigidbody.rotation = Quaternion.LookRotation(playerToMouse);
+            }
+#else
+
+            Vector3 turnDir =
+new Vector3(CrossPlatformInputManager.GetAxisRaw("Mouse X") , 0f , CrossPlatformInputManager.GetAxisRaw("Mouse Y"));
+
+            if (turnDir != Vector3.zero)
+            {
+                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+                Vector3 playerToMouse = (transform.position + turnDir) - transform.position;
+
+                // Ensure the vector is entirely along the floor plane.
+                playerToMouse.y = 0f;
+
+                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+                Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
+
+                // Set the player's rotation to this new rotation.
+                playerRigidbody.MoveRotation(newRotatation);
+            }
+#endif
+        }
+
+        public virtual void RotateWithAnotherTransform(Transform referenceTransform)
+        {
+            var newRotation = new Vector3(transform.eulerAngles.x, referenceTransform.eulerAngles.y,
+                transform.eulerAngles.z);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(newRotation),
+                strafeRotationSpeed * Time.fixedDeltaTime);
+            targetRotation = transform.rotation;
+        }
+    }
+}
